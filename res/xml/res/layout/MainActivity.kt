57f8cbe34a13691cdf8e8d9c974639cef0948f79
Faskid404 <aliufawas21@gmail.com>
2026-03-13 12:46:32 +0100

    Add MainActivity to manage overlay permissions

---
+++ res/xml/res/layout/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.thunder.free
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.provider.Settings
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+            startActivity(Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION))
+        }
+
+        val serviceIntent = Intent(this, OverlayService::class.java)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(serviceIntent)
+        } else {
+            startService(serviceIntent)
+        }
+
+        val accIntent = Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS)
+        startActivity(accIntent)
+
+        finish()
+    }
+}
